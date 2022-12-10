2a7602018126334414592adbc271942e3fa3d657
Simon <szymon.stypka@o2.pl>
2022-12-10 18:41:39 +0100

    Move all necessary elements from token to transaction component, add token selection option to the new add transaction dialog

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog'
+import { TokenService } from './token/token.service';
+import { TransactionService } from './transaction.service';
 
 @Component({
   selector: 'app-transaction',
@@ -25,8 +27,34 @@ export class TransactionComponent implements OnInit {
   selector: 'add-transaction-dialog',
   templateUrl: 'add-transaction-dialog.html',
 })
-export class AddTransactionDialog {
-  constructor(public dialogRef: MatDialogRef<AddTransactionDialog>) {}
+export class AddTransactionDialog implements OnInit{
+
+  constructor(public dialogRef: MatDialogRef<AddTransactionDialog>, 
+    private transactionService: TransactionService,
+    private tokenService: TokenService) {
+      dialogRef.disableClose = true;
+    }
+
+  tokenNames: string[] = []
+  price?: number
+
+  ngOnInit(): void {
+    this.getTokenNames();
+  }
+
+  public getTokenNames(): void{
+    this.tokenService.getTokenNames()
+      .subscribe(result => this.tokenNames = result);
+  }
+
+  public getTokenPrice(name: string): void{
+    this.tokenService.getTokenPrice(name)
+    .subscribe(result => this.price = result.price);
+  }
+
+  public changeName(event: any){
+      this.getTokenPrice(event);
+  }
 
   save(){
     console.log("Transaction saved.");

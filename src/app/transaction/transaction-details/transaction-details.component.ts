20f2ba2e2bb2e58f5e0088fe54256573218c2c17
Simon <szymon.stypka@o2.pl>
2023-01-12 15:10:25 +0100

    style: add css for transaction details card

--- src/app/transaction/transaction-details/transaction-details.component.ts
+++ src/app/transaction/transaction-details/transaction-details.component.ts
@@ -1,12 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Transaction } from '../Transaction';
 
 @Component({
   selector: 'app-transaction-details',
   templateUrl: './transaction-details.component.html',
-  styleUrls: ['./transaction-details.component.css']
+  styleUrls: ['./transaction-details.component.css'],
 })
-export class TransactionDetailsComponent {
-
+export class TransactionDetailsComponent{
+  
   @Input() clickedElement?: Transaction
+ 
+
+  getPossibleBalance(): string{
+    let str = "";
+    if(this.clickedElement){
+      let balance = this.clickedElement.amount + this.clickedElement.profit;
+      let profit = this.calculateProfitPercentage(this.clickedElement.profit, this.clickedElement.amount);
+      switch(profit > 0){
+        case true:
+          str = `${balance.toFixed(2)}$ (+${profit.toFixed(2)}%)`;
+          break;
+        case false:
+          str = `${balance.toFixed(2)}$ (-${profit.toFixed(2)}%)`;
+      }
+    }
+    return str;
+  }
+
+  calculateProfitPercentage(profit: number, costPrice: number): number{
+    return profit/costPrice * 100;
+  }
+
 }
